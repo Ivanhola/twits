9a82fac97d5c36a4ce8ce70b1387c6ce226fa764
ivanhola <ivanholalshs@gmail.com>
2021-12-11 13:36:27 -0800

    Added search function and styling

--- base/views.py
+++ base/views.py
@@ -1,23 +1,30 @@
 from django import forms
 from django.shortcuts import render, redirect
-from . models import Room
+from django.db.models import Q
+from . models import Room, Topic
 from . forms import RoomForm
-# rooms = [
-#      {'id':1 , 'name':'Lets learn python!'},
-#      {'id':2 , 'name':'Design with me'},
-#      {'id':3 , 'name':'Front end developers'},
-#  ]
+
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms' : rooms}
+    
+    #Main list data being shown
+    q = request.GET.get('q') if request.GET.get('q') != None else '' #filters query with q
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | #get the value of the variable (name) in topic obj
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    room_count = rooms.count()
+
+    #This is for the side bar to display
+    topics = Topic.objects.all()
+
+    #Context is the data being passed into the HTML page
+    context = {'rooms' : rooms , "topics": topics, "room_count": room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    # room = None
-    # for i in rooms:
-    #     if i['id'] == int(pk):
-    #         room = i
+
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, "base/room.html", context)
